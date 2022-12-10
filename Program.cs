5b9e44e32956988be099ba9a38c1f03f7c8a85ba
Gert De Niels <gert.deniels@hotmail.be>
2022-12-11 00:31:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BL;
+
+Console.WriteLine("Enter path:");
+
+string? filePath = Console.ReadLine();
+
+if (!string.IsNullOrEmpty(filePath))
+{
+    List<BO.Elf>? list = Elf.ReadFileElfData(filePath);
+
+    if (list == null) Console.WriteLine(0);
+    else Console.WriteLine(Elf.GetHighestAmountOfCaloriesForListOfElves(list));
+}
